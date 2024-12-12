e35001f229b7183d3a4aab08f30e022b8fbdd810
Walber Costa <walberoc@gmail.com>
2024-12-12 16:40:32 -0300

    inicia parte com medição

--- av1/testes/medindo_complexidade.py
+++ av1/testes/medindo_complexidade.py
@@ -3,4 +3,19 @@ import numpy as np
 import time
 
 # função para coletar tempo de execução
-
+def medindo_tempo(sizes):
+    times = []
+    for n in sizes:
+        # cria uma lista com n elementos
+        lista = list(range(n))
+        
+        # mede o tempo de execução
+        inicio = time.perf_counter()
+        
+        # executa insertion_sort(lista)
+        
+        fim = time.perf_counter()
+        # calcula o tempo de execução
+        tempo = fim - inicio
+        times.append(tempo)
+    return times
