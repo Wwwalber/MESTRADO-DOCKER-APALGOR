52e561e4c4bf00ea1ccd17bee721852a9951fd30
Walber Costa <walberoc@gmail.com>
2024-12-28 22:09:50 -0300

    gráficos comparativos

--- av1/scripts2/graficos2.py
+++ av1/scripts2/graficos2.py
@@ -13,11 +13,26 @@ def plotar_grafico(x, y):
 
 def plotar_grafico_insertion_sort(n, f_de_n_aleatorio, f_de_n_crescente, f_de_n_decrescente, titulo="Gráfico de Insertion Sort"):
     plt.figure(figsize=(10, 6))
-    plt.plot(n, f_de_n_aleatorio, label='Dados Aleatórios')
-    plt.plot(n, f_de_n_crescente, label='Dados Crescentes')
-    plt.plot(n, f_de_n_decrescente, label='Dados Decrescentes')
+    if f_de_n_aleatorio is not None:
+        plt.plot(n, f_de_n_aleatorio, label='Dados Aleatórios')
+    if f_de_n_crescente is not None:
+        plt.plot(n, f_de_n_crescente, label='Dados Crescentes')
+    if f_de_n_decrescente is not None:
+        plt.plot(n, f_de_n_decrescente, label='Dados Decrescentes')
     plt.xlabel('Tamanho do vetor (n)')
-    plt.ylabel('Tempo de execução (f(n))')
+    plt.ylabel('Tempo de execução f(n)')
+    plt.title(titulo)
+    #plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def plotar_grafico_comparativo_insertion_sort(n, f_de_n_aleatorio, f_de_n_crescente, f_de_n_decrescente, titulo="Comparação de Dados de Ordenação"):
+    plt.figure(figsize=(10, 6))
+    plt.plot(n, f_de_n_aleatorio, label='Dados iniciais Aleatórios', color='blue')
+    plt.plot(n, f_de_n_crescente, label='Dados iniciais Crescentes', color='red')
+    plt.plot(n, f_de_n_decrescente, label='Dados iniciaisS Decrescentes', color='green')
+    plt.xlabel('Tamanho do vetor (n)')
+    plt.ylabel('Tempo de execução f(n)')
     plt.title(titulo)
     plt.legend()
     plt.grid(True)
